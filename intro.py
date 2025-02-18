fee761b168edf95ad8c7e7235a3cd4799cf9ef46
SAHAL <sahal@ku.du.so>
2025-02-18 23:23:11 +0300

    one function addition

--- intro.py
+++ intro.py
@@ -12,3 +12,12 @@ def sayHi():
     name = input("enter ur name:")
     print(f"Hi Mr.{name}")
 sayHi() 
+
+def add(a, b):
+    return a+b 
+
+if __name__ == "__main__":
+    name1 = 5
+    name2 = 3
+    result = add(name1, name2)
+    print (" your result is :", result)
