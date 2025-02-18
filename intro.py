7f9a991ee920cbca46e659956c601b267a58584e
SAHAL <sahal@ku.du.so>
2025-02-18 23:29:49 +0300

    added sum fun

--- intro.py
+++ intro.py
@@ -11,3 +11,12 @@ def sayHi():
     name = input("enter ur name:")
     print(f"Hi Mr.{name}")
 sayHi() 
+
+def add(a,b):
+    return  a+ b
+
+if __name__ == "__main__":
+    nam1 = 5
+    nam2 = 7
+    result = add(nam1 , nam2)
+    print("your sum is :", result)
